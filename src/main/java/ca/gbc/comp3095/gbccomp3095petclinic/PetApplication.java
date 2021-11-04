d2887fe563cc486978e5296ee1ff21767eb8bd51
aryanluther <aryanluthra2014@gmail.com>
2021-11-04 10:04:52 -0400

    Initial commit

---
+++ src/main/java/ca/gbc/comp3095/gbccomp3095petclinic/PetApplication.java
@@ -0,0 +1,13 @@
+package ca.gbc.comp3095.gbccomp3095petclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ca/gbc/comp3095/gbccomp3095petclinic/PetApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.gbc.comp3095.gbccomp3095petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
